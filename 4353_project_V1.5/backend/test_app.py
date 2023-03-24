bd529ad9aa5d558f3c889abc0538eca415e4c3cb
JumpyJumpCATS <100959775+JumpyJumpCATS@users.noreply.github.com>
2023-03-24 00:18:57 -0500

    Added work for register, user, and logout

--- 4353_project_V1.5/backend/test_app.py
+++ 4353_project_V1.5/backend/test_app.py
@@ -15,6 +15,18 @@ class TestApp(TestCase):
         response = self.client.get(url_for('home'))
         self.assertEqual(response.status_code, 200)
         
+    def test_register(self):
+        response = self.client.get(url_for('register'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_user(self):
+        response = self.client.get(url_for('user'))
+        self.assertEqual(response.status_code, 302)
+
+    def test_logout(self):
+        response = self.client.get(url_for('logout'))
+        self.assertEqual(response.status_code, 302)
+        
 
 
 if __name__ == '__main__':
